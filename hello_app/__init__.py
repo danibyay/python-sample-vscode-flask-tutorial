140755aaf54757a1275779dd2e929519b21378a9
Daniela Becerra Gonzalez <danybecerr@gmail.com>
2020-05-18 20:39:35 -0500

    add login func

--- hello_app/__init__.py
+++ hello_app/__init__.py
@@ -3,9 +3,13 @@ from .config import Config
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from flask_migrate import Migrate
+from flask_login import LoginManager
+
 
 app = Flask(__name__)    # Create an instance of the class for our use
 app.config.from_object(Config)
 engine_azure = create_engine(app.config['SQLALCHEMY_DATABASE_URI'],echo=True)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
