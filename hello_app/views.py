722cbb93b8ee24708ff1f46a3e5ae683345b83a5
Daniela Becerra Gonzalez <danybecerr@gmail.com>
2020-05-24 17:11:25 -0500

    improve user profile page and add DB User fields

--- hello_app/views.py
+++ hello_app/views.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template, flash, redirect, url_for, request
 from . import app
 from . import db
 from .models import User
-from .forms import LoginForm, RegistrationForm
+from .forms import LoginForm, RegistrationForm, EditProfileForm
 from flask_login import current_user, login_user
 from flask_login import logout_user
 from flask_login import login_required
 from werkzeug.urls import url_parse
+from datetime import datetime
+
 
 @app.route("/")
 @app.route('/index')
@@ -70,3 +72,25 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
