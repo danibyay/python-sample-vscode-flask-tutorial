014d46989787b95522eb5a1d07e1d35c7ca1a728
Daniela Becerra Gonzalez <danybecerr@gmail.com>
2020-05-24 17:41:28 -0500

    Simply add and display all posts

--- hello_app/views.py
+++ hello_app/views.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, flash, redirect, url_for, request
 from . import app
 from . import db
-from .models import User
-from .forms import LoginForm, RegistrationForm, EditProfileForm
+from .models import User, Post
+from .forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
 from flask_login import current_user, login_user
 from flask_login import logout_user
 from flask_login import login_required
@@ -10,22 +10,29 @@ from werkzeug.urls import url_parse
 from datetime import datetime
 
 
-@app.route("/")
-@app.route('/index')
+@app.route("/", methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    #user = {'username': 'Daniela'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool'
-        }
-    ]
-    return render_template('index.html', title='Home', posts=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    # posts = [
+    #     {
+    #         'author': {'username': 'John'},
+    #         'body': 'Beautiful day in Portland!'
+    #     },
+    #     {
+    #         'author': {'username': 'Susan'},
+    #         'body': 'The Avengers movie was so cool'
+    #     }
+    # ]
+    posts = Post.query.all()
+    return render_template('index.html', title='Home Page', form=form, posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
